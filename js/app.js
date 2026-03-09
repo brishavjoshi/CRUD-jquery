0e08dcdaebd1d1848567f98b2ec415ce44596cd3
brishavjoshi <brishavjoshi2081@gmail.com>
2026-03-09 14:23:35 +0545

    app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
-var items = groceryItems;
 
-// Render App
+var items = groceryItems; 
+
+function editCompleted(itemId) {
+  items = $.map(items, function (item) {
+    if (item.id === itemId) {
+  
+      return $.extend({}, item, { completed: !item.completed });
+    }
+    return item;
+  });
+  render(); 
+}
+
+
 function render() {
   var $app = $("#app");
   $app.empty();
 
   var $itemsElement = createItems(items);
+
+  $itemsElement.find('.single-item').each(function(index) {
+    var itemId = items[index].id;
+    $(this).find('input[type="checkbox"]').on('change', function() {
+      editCompleted(itemId);
+    });
+  });
+
   $app.append($itemsElement);
 }
 
-// Initialize App
+// 4. Initialize App
 $(document).ready(function () {
   render();
 });
