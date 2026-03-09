22a151ebf3b9b0796b3f0edd8c689c87316a82b6
brishavjoshi <brishavjoshi2081@gmail.com>
2026-03-09 15:31:25 +0545

    single-item.js

--- js/single-item.js
+++ js/single-item.js
@@ -1,27 +1,34 @@
-
+// Create SingleItem Element
 function createSingleItem(item) {
   var $div = $('<div class="single-item"></div>');
 
-
+  // 1. Template with dynamic classes/styles
   $div.html(`
     <input type="checkbox" ${item.completed ? "checked" : ""} />
     <p style="text-decoration: ${item.completed ? "line-through" : "none"}">
       ${item.name}
     </p>
-    <button class="btn icon-btn edit-btn" type="button">
-      <i class="fa-regular fa-pen-to-square"></i>
-    </button>
-    <button class="btn icon-btn remove-btn" type="button">
-      <i class="fa-regular fa-trash-can"></i>
-    </button>
+    <div class="btn-container">
+      <button class="btn icon-btn edit-btn" type="button">
+        <i class="fa-regular fa-pen-to-square"></i>
+      </button>
+      <button class="btn icon-btn remove-btn" type="button">
+        <i class="fa-regular fa-trash-can"></i>
+      </button>
+    </div>
   `);
 
-
+  // 2. Event: Checkbox Toggle (Completed)
   $div.find('input[type="checkbox"]').on("change", function () {
     editCompleted(item.id);
   });
 
-  
+  // 3. Event: Edit Button (Switch to Edit Mode)
+  $div.find(".edit-btn").on("click", function () {
+    setEditId(item.id);
+  });
+
+  // 4. Event: Remove Button (Delete)
   $div.find(".remove-btn").on("click", function () {
     removeItem(item.id);
   });
